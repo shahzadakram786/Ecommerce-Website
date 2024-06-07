4edc0bbbff1b29efa17f9e9917ab3f584f96eda8
shahzadakram786 <alyakram786@gmail.com>
2024-06-07 23:59:18 +0500

    ,

--- ecommerce/src/components/common/Header.jsx
+++ ecommerce/src/components/common/Header.jsx
@@ -2,8 +2,13 @@ const Header = () => {
   return (
     <div>
       <header className="header px-12 py-3 bg-white-100 relative z-20">
-        <h1>Ecommerce</h1>
-        <p>This is my ecommerce react app with redux</p>
+        <div className="p-4 flex justify-between items-center relative">
+          <div className="flex items-center gap-14">
+            <div>
+              <h1>Ecommerce</h1>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
