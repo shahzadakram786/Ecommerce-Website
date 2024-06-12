16923b43976f8cce87828a425f15de74e6f57711
shahzadakram786 <alyakram786@gmail.com>
2024-06-12 13:32:43 +0500

    hero section almost completed now working on its responsive ness

--- ecommerce/src/components/hero/Hero.jsx
+++ ecommerce/src/components/hero/Hero.jsx
@@ -2,21 +2,67 @@ import { useState } from "react";
 import { herolist } from "../../assets/data/data";
 import { BodyOne, Caption, Title } from "../common/CustomComponents";
 import PropTypes from "prop-types";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+
+function SampleNextArrow(props) {
+  const { onClick } = props;
+  return (
+    <div
+      className="absolute bottom-0 bg-black text-white left-96 lg:left-1/2 slider-btn"
+      onClick={onClick}
+    >
+      <button className="next">
+        <MdKeyboardArrowRight size={50} />
+      </button>
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { onClick } = props;
+  return (
+    <div
+      className="absolute bottom-0 bg-white text-primary left-96 lg:left-[46.5%] border-[1px] border-black slider-btn z-10"
+      onClick={onClick}
+    >
+      <button className="next">
+        <MdKeyboardArrowLeft size={50} />
+      </button>
+    </div>
+  );
+}
 
 const Hero = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+
   return (
     <div>
-      <section className="h-[50vh] lg:h-[90vh] mt-20 bg-white-100 relative z-1">
-        {herolist.slice(0, 1).map((item) => (
-          <HeroItem
-            key={item.id}
-            title={item.title}
-            description={item.description}
-            prices={item.price}
-            colors={item.color}
-            image={item.image}
-          />
-        ))}
+      <section
+        className="h-[50vh] lg:h-[90vh] mt-20  relative z-1 
+      "
+      >
+        <Slider {...settings}>
+          {herolist.map((item) => (
+            <HeroItem
+              key={item.id}
+              title={item.title}
+              description={item.description}
+              prices={item.price}
+              colors={item.color}
+              image={item.image}
+            />
+          ))}
+        </Slider>
       </section>
     </div>
   );
@@ -81,7 +127,7 @@ export const HeroItem = ({ title, description, prices, colors, image }) => {
             className="h-[60vh] w-full object-contain"
           />
         </div>
-        <div className="lg:bg-black lg:h-[96vh] lg:absolute lg:top-0 lg:right-0 lg:w-1/3 lg:-z-10"></div>
+        <div className="lg:bg-black lg:h-[90vh] lg:absolute lg:top-0 lg:right-0 lg:w-1/3 lg:-z-10"></div>
       </section>
     </>
   );
