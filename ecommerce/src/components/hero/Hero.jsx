a2fb624f56bf375146ab932d4c39129ffbcaef24
shahzadakram786 <alyakram786@gmail.com>
2024-07-08 23:47:45 +0500

    debuggingg

--- ecommerce/src/components/hero/Hero.jsx
+++ ecommerce/src/components/hero/Hero.jsx
@@ -133,48 +133,48 @@ export const HeroItem = ({ title, description, prices, colors, image }) => {
   );
 };
 
-export const Banner = () => {
-  <>
-    <div className="py-20 container flex flex-col lg:flex-row items-center gap-5">
-      <div>
-        <BannerCard
-          title="Wooden Water Bottles"
-          desc="UP TO 60% OFF"
-          cover="./images/hero/product1-1.png"
-        />
-      </div>
-      <div className="flex justify-center flex-col gap-8">
-        <BannerCard title="" desc="" cover="./images/hero/product2.png" />
-        <BannerCard title="" desc="" cover="./images/hero/product3.png" />
-      </div>
-    </div>
-  </>;
-};
+// export const Banner = () => {
+//   <>
+//     <div className="py-20 container flex flex-col lg:flex-row items-center gap-5">
+//       <div>
+//         <BannerCard
+//           title="Wooden Water Bottles"
+//           desc="UP TO 60% OFF"
+//           cover="./images/hero/product1-1.png"
+//         />
+//       </div>
+//       <div className="flex justify-center flex-col gap-8">
+//         <BannerCard title="" desc="" cover="./images/hero/product2.png" />
+//         <BannerCard title="" desc="" cover="./images/hero/product3.png" />
+//       </div>
+//     </div>
+//   </>;
+// };
 
-const BannerCard = ({ title, desc, cover, className, classSecond }) => {
-  return (
-    <>
-      <div className="w-full h-full relative">
-        <img src={cover} alt="" />
-        <div
-          className={`${
-            className
-              ? "absolute bottom-0 p-8 w-full"
-              : "flex absolute bottom-0 p-8 w-full"
-          }${className && classSecond ? "left-0 lg:left-48 top-0 w-96" : ""}`}
-        >
-          <div>
-            <Title level={2}>{title}</Title>
-            <p className="text-lg font-normal leading-none">{desc} </p>
-          </div>
-          <div className="w-1/2 mt-5">
-            <button className="secondry-btn flex justify-end">Shop now</button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+// const BannerCard = ({ title, desc, cover, className, classSecond }) => {
+//   return (
+//     <>
+//       <div className="w-full h-full relative">
+//         <img src={cover} alt="" />
+//         <div
+//           className={`${
+//             className
+//               ? "absolute bottom-0 p-8 w-full"
+//               : "flex absolute bottom-0 p-8 w-full"
+//           }${className && classSecond ? "left-0 lg:left-48 top-0 w-96" : ""}`}
+//         >
+//           <div>
+//             <Title level={2}>{title}</Title>
+//             <p className="text-lg font-normal leading-none">{desc} </p>
+//           </div>
+//           <div className="w-1/2 mt-5">
+//             <button className="secondry-btn flex justify-end">Shop now</button>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 
 export default Hero;
 
--- ecommerce/src/components/product/Product.jsx
+++ ecommerce/src/components/product/Product.jsx
@@ -1,14 +1,14 @@
-import { Title } from "../common/CustomComponents";
+// import { Title } from "../common/CustomComponents";
 
-export const Product = () => {
-  return (
-    <>
-      <section className="py-20 bg-white">
-        <div className="container">
-          <Title level={4}>Most Popular Product</Title>
-          <div className="flex items-center gap-3 uppercase"></div>
-        </div>
-      </section>
-    </>
-  );
-};
+// export const Product = () => {
+//   return (
+//     <>
+//       <section className="py-20 bg-white">
+//         <div className="container">
+//           <Title level={4}>Most Popular Product</Title>
+//           <div className="flex items-center gap-3 uppercase"></div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
