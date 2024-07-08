19645ae20cca6acfb4c54715d2614638c7ec670d
shahzadakram786 <alyakram786@gmail.com>
2024-07-08 22:54:29 +0500

    working on errors

--- ecommerce/src/components/product/Product.jsx
+++ ecommerce/src/components/product/Product.jsx
@@ -6,6 +6,7 @@ export const Product = () => {
       <section className="py-20 bg-white">
         <div className="container">
           <Title level={4}>Most Popular Product</Title>
+          <div className="flex items-center gap-3 uppercase"></div>
         </div>
       </section>
     </>
